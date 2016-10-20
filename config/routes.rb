4baef89da2d80e99344aadd1bb0a9658227d6e57
guidoux <pedroguidoux@gmail.com>
2016-10-20 00:13:06 -0200

    Authenticated users can CRUD articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles
   devise_for :users
   get 'static_pages/about'
+  root 'articles#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019171854) do
+ActiveRecord::Schema.define(version: 20161019180910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -39,4 +48,5 @@ ActiveRecord::Schema.define(version: 20161019171854) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "articles", "users"
 end
